5607c8a5051ddf12794577d29aeb3ab81a4a15a4
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-03-02 17:34:38 -0500

    Starting lab 2 coins!!

--- src/lab2/CoinChanger.java
+++ src/lab2/CoinChanger.java
@@ -6,6 +6,8 @@
 
 package lab2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author class
@@ -16,7 +18,37 @@ public class CoinChanger {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       final int QUARTER_VALUE = 25;
+       final int DIME_VALUE = 10;
+       final int NICKLE_VALUE = 5;
+        String input = JOptionPane.showInputDialog("Enter the amount of change: ");
+        int d = Integer.parseInt(input);
+        System.out.println(" The user entered: " + d);
+        
+        // TODO: compute the amount of change
+        
+        //TODO: Calculate the numbers of quarters
+        int quarterCount = d/QUARTER_VALUE;
+        System.out.println(" I now have "+ quarterCount + "quarter" );
+        
+        d %= QUARTER_VALUE;
+        System.out.println("I have " + d + "left to go.");
+        
+        //TODO: Calculate the number of dimes
+        int dimeCount;
+        
+        
+        //TODO: Calculate the number of nickels
+        int nickelCount = 0;
+        
+        //TODO: What's left in d is the amount of pennies.
+        //TODO: Print the number of coins in each categories.
+        //TODO: Print the total number of coins.
+        
+        
+        
+  
     }
     
 }
