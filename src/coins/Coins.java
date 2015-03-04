ed879579d469709e778448d7ef92c36407001917
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-03-04 16:34:56 -0500

    Lab 2 coin change

---
+++ src/coins/Coins.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package coins;
+
+/**
+ *
+ * @Niaz Morshed and Devya Gurung
+ * @version 03/3/2015
+ */
+import javax.swing.JOptionPane;
+
+public class Coins {
+
+    public static void main(String[] args) {
+        // create constant variable
+        final int QUARTER_VALUE = 25;
+        final int DIME_VALUE = 10;
+        final int NICKLE_VALUE = 5;
+        final int PENNY_VALUE = 1;
+
+        // receive the amount
+        String input = JOptionPane.showInputDialog("Enter the amount of change: ");
+        int d = Integer.parseInt(input);
+        System.out.println(" The user entered: " + d);
+
+        //Calculate the numbers of quarters
+        int quarterCount = d / QUARTER_VALUE;
+        System.out.println("Quarters: " + quarterCount);
+
+        //calculate the remaining amount
+        d %= QUARTER_VALUE;
+
+        //Calculate the number of dimes
+        int dimeCount = d / DIME_VALUE;
+        System.out.println("Dimes: " + dimeCount);
+
+        //calculate the remaining amount
+        d %= DIME_VALUE;
+
+        //Calculate the number of nickels
+        int nickleCount = d / NICKLE_VALUE;
+        System.out.println("Nickles: " + nickleCount);
+
+        //calculate the remaining amount
+        d %= NICKLE_VALUE;
+
+        //Calculate the number of pennies
+        int pennyCount = d / PENNY_VALUE;
+        System.out.println("Pennies: " + pennyCount);
+
+        // calculate the total number of coins used
+        int total = quarterCount + dimeCount + nickleCount + pennyCount;
+        System.out.println("The total number of coins: " + total);
+        JOptionPane.showMessageDialog(null, " Please take your change : \n " + "Quarters: " + quarterCount + "\n"
+                + "Dimes: " + dimeCount + "\n" + "Nickles: " + nickleCount + "\n"
+                + "Pennies: " + pennyCount + "\n" + "The total number of coins: " + total);
+
+    }
+
+
+}
